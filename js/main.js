31ab4023e3d90b2359bd4230a909aa6243584b4c
3axap4eHko <3axap4eHko@gmail.com>
2013-10-13 23:05:28 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    Cufon.replace('h1, h2, h3, li, span, .cufon-text');
+    Cufon.replace('h1, h2, h3, li, p, .cufon-text');
 
     $('.print-vc').click(function(){
         window.print();
